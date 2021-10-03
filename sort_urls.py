54228f0c41882f1a9708ab5761e9168e4e03387b
Holly Liu <mistladylan@gmail.com>
2021-10-03 13:11:12 +1100

    update files

--- sort_urls.py
+++ sort_urls.py
@@ -1,21 +1,23 @@
 import requests
 import sys
+import json
 
 URL = sys.argv[1]
 
-response = requests.get(URL)
-data = response.text
-lines = data.split('\n')
+def lambda_handler(event, context):
+    response = requests.get(URL)
+    data = response.text
+    lines = data.split('\n')
 
-url_count = {}
-for line in lines:
-    try:
-        url = line.split(' ')[6]
-        if url not in url_count:
-            url_count[url] = 0
-        url_count[url] += 1
-    except:
-        pass
-        
-sorted_url_count = sorted(url_count.items(), key=lambda x: x[1], reverse=True)
-print(sorted_url_count[:10])
+    url_count = {}
+    for line in lines:
+        try:
+            url = line.split(' ')[6]
+            if url not in url_count:
+                url_count[url] = 0
+            url_count[url] += 1
+        except:
+            pass
+
+    sorted_url_count = sorted(url_count.items(), key=lambda x: x[1], reverse=True)
+    print(sorted_url_count[:10])
